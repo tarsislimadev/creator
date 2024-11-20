e5a13f542c22ebde199c043d10775210c54595ab
Tarsis Lima <br.tmvdl@gmail.com>
2024-11-20 19:59:19 -0300

    Updating app files

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,23 @@
-console.log('creator')
+const express = require('express')
+const { createServer } = require('http')
+const { Server } = require('socket.io')
+const { EventEmitter } = require('stream')
+
+const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer, {})
+const ee = new EventEmitter()
+app.use(express.static('public'))
+
+const io_events = []
+const socket_events = ['message']
+
+io.on('connection', (socket) => {
+  Array.from(socket_events)
+    .map((sev) => socket.on(sev, (...data) => ee.emit('socket:' + sev, ...data)))
+})
+
+Array.from(io_events)
+  .map((sev) => io.on(sev, (...data) => ee.emit('io:' + sev, ...data)))
+
+httpServer.listen(80, () => console.log(`Listening on port 80`))
