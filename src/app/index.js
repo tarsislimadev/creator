f8f766f7d93fd80789850f94cfb8344a8208b1c7
Tarsis Lima <br.tmvdl@gmail.com>
2024-11-20 20:43:51 -0300

    Updating app files

--- src/app/index.js
+++ src/app/index.js
@@ -9,15 +9,21 @@ const io = new Server(httpServer, {})
 const ee = new EventEmitter()
 app.use(express.static('public'))
 
-const io_events = []
+const io_events = ['message']
 const socket_events = ['message']
+
+setInterval(() => ee.emit('socket:message', Date.now()), 1000)
+
+ee.addListener('socket:message', (d) => console.log('socket:message', d))
+
+Array.from(socket_events)
+  .map((sev) => ee.addListener('socket:' + sev, (...data) => console.log('socket:' + sev, ...data)))
 
 io.on('connection', (socket) => {
   Array.from(socket_events)
     .map((sev) => socket.on(sev, (...data) => ee.emit('socket:' + sev, ...data)))
+  Array.from(io_events)
+    .map((sev) => ee.addListener('socket:' + sev, (...data) => socket.emit(sev, ...data)))
 })
-
-Array.from(io_events)
-  .map((sev) => io.on(sev, (...data) => ee.emit('io:' + sev, ...data)))
 
 httpServer.listen(80, () => console.log(`Listening on port 80`))
