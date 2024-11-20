0007e29f56f73ee92713cd55fab76d0d14a70940
Tarsis Lima <br.tmvdl@gmail.com>
2024-11-20 20:43:12 -0300

    Updating app files

--- src/app/public/index.js
+++ src/app/public/index.js
@@ -1,1 +1,19 @@
-alert('creator')
+import { io } from './libs/socket/index.js'
+import { PageComponent } from './js/components/page.component.js'
+import { TextComponent } from './js/components/text.component.js'
+
+export class Page extends PageComponent {
+  state = {
+    socket: io(),
+    ee: new EventTarget(),
+  }
+
+  setEvents() {
+    this.state.socket.on('message', (m) => this.append(new TextComponent({ text: m })))
+  }
+
+  setBody() {
+    this.append(new TextComponent({ text: 'body text' }))
+  }
+
+}
