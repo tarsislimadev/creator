e17b3b0306e30ca8111f408c99c6776d9f591f85
Tarsis Lima <br.tmvdl@gmail.com>
2024-11-23 12:48:16 +0000

    Updating app files

--- src/app/public/index.js
+++ src/app/public/index.js
@@ -1,11 +1,24 @@
 import { io } from './libs/socket/index.js'
 import { PageComponent } from './js/components/page.component.js'
 import { TextComponent } from './js/components/text.component.js'
+import { ButtonComponent } from './js/components/button.component.js'
+import { SocketMessageModel } from './js/models/socket.message.model.js'
+import { RecordScreenSocketMessageModel } from './js/models/message/socket/record.screen.js'
+import { RecordCamSocketMessageModel } from './js/models/message/socket/record.cam.js'
 
 export class Page extends PageComponent {
   state = {
     socket: io(),
     ee: new EventTarget(),
+    messages: [],
+  }
+
+  onRecordScreenButtonClick() {
+    this.sendSocketMessage(new RecordScreenSocketMessageModel()) // ('record:screen')
+  }
+
+  onRecordCamButtonClick() {
+    this.sendSocketMessage(new RecordCamSocketMessageModel())
   }
 
   setEvents() {
@@ -14,6 +27,13 @@ export class Page extends PageComponent {
 
   setBody() {
     this.append(new TextComponent({ text: 'body text' }))
+    this.append(new ButtonComponent({ text: 'record cam', onclick: () => this.onRecordCamButtonClick() }))
+    this.append(new ButtonComponent({ text: 'record cam', onclick: () => this.onRecordCamButtonClick() }))
+  }
+
+  sendSocketMessage(message = new SocketMessageModel('input')) {
+    this.state.messages.push(message)
+    this.state.socket.send('message', message.toString())
   }
 
 }
